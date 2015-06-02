3ec58854285fb9532b4d14d36503877d56888932
bbdev001 <developer@192.168.1.92>
2015-06-02 11:23:20 +0300

    Some experiments

--- src/com/my/fly/RoutesActivity.java
+++ src/com/my/fly/RoutesActivity.java
@@ -596,14 +596,16 @@ public class RoutesActivity extends Activity implements OnItemClickListener, Loc
 				WayPoint wp = wayPoints.get(i);
 				DJIGroundStationWaypoint gsWayPoint = new DJIGroundStationWaypoint(wp.coord.Lat, wp.coord.Lon, 3, 1);
 				gsWayPoint.altitude = (float) wp.Alt;
-				gsWayPoint.heading = 0;//wp.Heading;
+				gsWayPoint.heading = 179;//wp.Heading;
 				gsWayPoint.speed = (float) wp.Speed;
 				gsWayPoint.maxReachTime = (short) wp.MaxReachTime;
 				gsWayPoint.stayTime = (short) wp.HoverTime;
 				gsWayPoint.turnMode = wp.HoverTime > 0 ? 0 : 2;
 				gsWayPoint.hasAction = true;
 
-				gsWayPoint.addAction(GroundStationOnWayPointAction.Way_Point_Action_Craft_Yaw, 90/*this.ConvertHeading(wp.Heading)*/);
+				gsWayPoint.addAction(GroundStationOnWayPointAction.Way_Point_Action_Craft_Yaw, 45/*this.ConvertHeading(wp.Heading)*/);
+				gsWayPoint.addAction(GroundStationOnWayPointAction.Way_Point_Action_Gimbal_Yaw, 90);
+				gsWayPoint.addAction(GroundStationOnWayPointAction.Way_Point_Action_Gimbal_Pitch, -45);
 				gsWayPoint.addAction(GroundStationOnWayPointAction.Way_Point_Action_Simple_Shot, 1);
 				gsWayPoint.addAction(GroundStationOnWayPointAction.Way_Point_Action_Stay, wp.HoverTime * 10);
                 
