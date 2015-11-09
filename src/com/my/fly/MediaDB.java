1c826131a4227e81888b7e9ae0cf0c505753ca0b
bbdev001 <alexbest@fromru.com>
2015-11-10 01:06:37 +0300

    Added images storing to database

--- src/com/my/fly/MediaDB.java
+++ src/com/my/fly/MediaDB.java
@@ -17,87 +17,101 @@ public class MediaDB
 	protected String path;
 	protected RandomAccessFile file = null;
 	protected MediaDBSql sqlDb = null;
-	
+
 	public MediaDB(Context context, String path)
 	{
-        File dbDir = new File(path);
-        if (!dbDir.exists()) 
-            dbDir.mkdirs();
-        
-        this.path = path;
-        
-        sqlDb = new MediaDBSql(context, path + "pictures.db", 1);
+		File dbDir = new File(path);
+		if (!dbDir.exists())
+			dbDir.mkdirs();
+
+		this.path = path;
+
+		sqlDb = new MediaDBSql(context, path + "pictures.db", 1);
 	}
-	
+
 	public boolean HasFile(String fileName)
 	{
 		File destCheck = new File(path + fileName);
-		
-        return destCheck.exists();	
+
+		return destCheck.exists();
 	}
-	
+
 	public String GetCurrentFileName()
 	{
 		return fileName;
 	}
-	
+
 	public void OpenFile(String fileName)
 	{
 		if (IsOpened())
 			CloseFile();
 
-		try 
+		try
 		{
 			this.fileName = fileName;
 			String fullFileName = path + fileName;
-            File destCheck = new File(fullFileName);
-            
-            if (destCheck.exists())
-                destCheck.delete();
-            
-            file = new RandomAccessFile(fullFileName, "rw");
-        } 
-		catch (IOException e) 
+			File destCheck = new File(fullFileName);
+
+			if (destCheck.exists())
+				destCheck.delete();
+
+			file = new RandomAccessFile(fullFileName, "rw");
+		}
+		catch (IOException e)
 		{
 			e.printStackTrace();
-        }
+		}
 	}
-	
+
 	public void WriteFileBlock(byte[] buffer, int bufferSize)
 	{
 		try
 		{
 			file.write(buffer, 0, bufferSize);
 		}
-		catch (IOException e) 
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void CommitFiles(ArrayList<String> files)
 	{
-		ExifInterface exifInterface = null;
-		
-		for(int i = 0; i < files.size(); i++)
+		try
 		{
-			try 
+			sqlDb.PrepareInsertion();
+
+			ExifInterface exifInterface = null;
+			float[] coords = new float[2];
+
+			for (int i = 0; i < files.size(); i++)
 			{
 				exifInterface = new ExifInterface(path + files.get(i));
-				float[] coords = null;
+				double altInMeters = exifInterface.getAltitude(0.0);
+
 				if (exifInterface.getLatLong(coords))
 				{
+					int latInt = (int) (coords[0] * 100000.0f);
+					int lonInt = (int) (coords[1] * 100000.0f);
+					int altInt = (int) altInMeters;
+
+					sqlDb.AddImageInfo(latInt, lonInt, altInt, files.get(i));
 				}
-				
-				double altInMeters = exifInterface.getAltitude(0.0);
 			}
-			catch (IOException ex) 
-			{
-				Log.e("MediaDB", "cannot read exif", ex);
-			}
+
+			sqlDb.DoneInsertion();
+		}
+		catch (Exception ex)
+		{
+			// Send error here
+			Log.e("MediaDB", "can't commit images", ex);
+		}
+		finally
+		{
+			sqlDb.CommitInsertion();
 		}
 	}
-	
+
 	public void RebuildIndexes()
 	{
 		File file = new File(path);
@@ -107,7 +121,7 @@ public class MediaDB
 			return;
 
 		ArrayList<String> filesToCommit = new ArrayList<String>();
-		
+
 		for (int i = 0; i < files.length; i++)
 		{
 			if (files[i].isDirectory())
@@ -116,15 +130,16 @@ public class MediaDB
 			String name = files[i].getName();
 			if (name.indexOf(".jpg") < 0)
 				continue;
-			
+
 			filesToCommit.add(name);
 		}
-		
+
+		sqlDb.Clear();
 		CommitFiles(filesToCommit);
 
 		filesToCommit.clear();
 	}
-	
+
 	public void CloseFile()
 	{
 		try
@@ -133,39 +148,79 @@ public class MediaDB
 			file.close();
 			file = null;
 		}
-		catch(IOException e)
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean IsOpened()
 	{
 		return file != null;
 	}
-	
-	public class MediaDBSql  extends SQLiteOpenHelper
+
+	public class MediaDBSql extends SQLiteOpenHelper
 	{
+		protected final String tableName = "pictures";
+		protected final String latLonIndexName = "picturesLatLon";
+
 		public MediaDBSql(Context context, String databaseFullPath, int version)
 		{
-			super(context, databaseFullPath , null, version);
+			super(context, databaseFullPath, null, version);
 		}
 
 		@Override
-		public void onCreate(SQLiteDatabase db) 
+		public void onCreate(SQLiteDatabase db)
 		{
-			db.execSQL("CREATE TABLE pictures (id integer primary key, latInt integer, lonInt integer, name text)");
+			db.execSQL("CREATE TABLE " + tableName + " (id integer primary key, latInt integer, lonInt integer, altInt integer, name text)");
 		}
 
 		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) 
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
 		{
-			db.execSQL("DROP TABLE IF EXISTS pictures");
+			db.execSQL("DROP TABLE IF EXISTS " + tableName);
 			onCreate(db);
-		}	
+		}
+
+		public void PrepareInsertion()
+		{
+			getWritableDatabase().beginTransaction();
+			getWritableDatabase().execSQL("CREATE INDEX IF NOT EXISTS " + latLonIndexName + " ON " + tableName + "(latInt, lonInt)");
+		}
+
+		public void DoneInsertion()
+		{
+			getWritableDatabase().setTransactionSuccessful();
+		}
+
+		public void CommitInsertion()
+		{
+			getWritableDatabase().endTransaction();
+		}
+
+		public void Clear()
+		{
+			try
+			{
+				getWritableDatabase().beginTransaction();
+				getWritableDatabase().execSQL("DROP INDEX IF EXISTS " + latLonIndexName);
+				getWritableDatabase().execSQL("DELETE * FROM " + tableName);
+				getWritableDatabase().setTransactionSuccessful();
+			}
+			finally
+			{
+				getWritableDatabase().endTransaction();
+			}
+		}
+
+		public void AddImageInfo(int lat, int lon, int alt, String name)
+		{
+			getWritableDatabase().execSQL("INSERT INTO " + tableName + " (latInt, lonInt, altInt, name) VALUES (" + lat + "," + lon + "," + alt + ",'" + name + "')");
+		}
+
+		public void Close()
+		{
+			this.close();
+		}
+	}
 }
-	
-}
-
-
-
